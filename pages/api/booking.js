07e95faccba9ccde5fa0db853fa32a4d40da3016
Ashley de Silva <ashley12.webdevelopment@gmail.com>
2023-03-31 02:22:19 +1100

    email functionality with sendgrid implemented

--- pages/api/booking.js
+++ pages/api/booking.js
@@ -1,6 +1,10 @@
 import { config } from "../../lib/sanity/config";
 import axios from "axios";
 
+const sgMail = require("@sendgrid/mail");
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 export default async function handler(req, res) {
   const projectId = config.projectId;
   const dataset = config.dataset;
@@ -17,6 +21,8 @@ export default async function handler(req, res) {
     eventCoordinates,
     kmsToDestination,
   } = req.body;
+
+  console.log(eventCoordinates);
 
   if (req.method !== "POST") {
     return res.status(404).json({ message: "invalid method type" });
@@ -67,6 +73,33 @@ export default async function handler(req, res) {
     );
 
     const bookingId = data.results[0].id;
+
+    //send email using sendgrid
+    const message = `
+        Name:${contactName}\r\n
+        email: ${contactEmail}\r\n
+        phone: ${contactNumber}\r\n\r\n
+        event Date: ${new Date(eventDate)}\r\n
+        event Address: ${eventAddress}\r\n
+        Event Coordinates: ${JSON.stringify(eventCoordinates)}\r\n
+        Head Count: ${headCount}\r\n\r\n
+        ${
+          additionalComments
+            ? `Additional Comments: ${additionalComments}\r\n`
+            : null
+        }
+        Kms To Detination: ${kmsToDestination}\r\n`;
+
+    const emailData = {
+      to: "tech@hoppersunlimited.com.au",
+      from: "bookings@hoppersunlimited.com.au",
+      subject: `New Booking - ${new Date()}`,
+      text: message,
+      html: message.replace(/\r\n/g, "<br>"),
+    };
+
+    await sgMail.send(emailData);
+
     res
       .status(201)
       .json({ _id: bookingId, message: "booking submitted successful" });
