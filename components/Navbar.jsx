0af8f6fafe6f24950cc186a200ad056a1d41f516
Ashley de Silva <ashley12.webdevelopment@gmail.com>
2023-04-04 14:08:59 +1000

    add link to navigation logo to the main site

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import logo from "../public/images/logo.png"
 import Image from 'next/image'
+import Link from "next/link";
 import styles from "../styles/Navbar.module.css"
 
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <div className={styles["nav-center"]}>
+        <Link href={`https://hoppersunlimited.com.au`}>
         <Image src={logo} alt="Hoppers Unlimited"/>
+        </Link>
       </div>
     </nav>
   )
